f714acebcc34076a0e79ce3f1d0be0ea05331393
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2020-08-31 22:12:47 -0700

    build: setup webpack & babel

---
+++ examples/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+
+import Hello from "../../src";
+
+render(
+  <div>
+    <Hello />
+  </div>,
+  document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: path.resolve(__dirname, "examples/src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "examples/dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "examples/src/index.html"),
+    }),
+  ],
+  devServer: {
+    compress: true,
+    port: 3000,
+    hot: true,
+    stats: "minimal",
+  },
+};
